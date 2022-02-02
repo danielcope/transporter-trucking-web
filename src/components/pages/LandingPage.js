f27cf43f42b0d8b8dcab2bb0069e835a55ff476d
Daniel Cope <danielcope@users.noreply.github.com>
2022-02-01 23:48:13 -0700

    removed mouse component

--- src/components/pages/LandingPage.js
+++ src/components/pages/LandingPage.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import AboutSection from "../AboutSection";
-import Mouse from "../Mouse";
 
 const LandingPage = () => {
   return (
